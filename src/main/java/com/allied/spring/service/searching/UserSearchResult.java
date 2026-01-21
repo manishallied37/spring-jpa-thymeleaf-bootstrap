e4b175a21c277038e5667757e71dce58329616c6
ManishK <manishkumar.c@alliedglobetech.com>
2026-01-21 14:14:33 +0530

    added changes and removed redundant code

--- src/main/java/com/allied/spring/service/searching/UserSearchResult.java
+++ src/main/java/com/allied/spring/service/searching/UserSearchResult.java
@@ -13,26 +13,26 @@ public class UserSearchResult {
     private Page<UserDto> userPage;
     private boolean numberFormatException;
 
-    public UserSearchResult(Page<UserDto> findAllPageable, boolean b) {
-        // TODO Auto-generated constructor stub
-    }
-
-    public UserSearchResult() {
-        // TODO Auto-generated constructor stub
-    }
-
-    public Page<UserDto> getUserPage() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public void setUserPage(Page<UserDto> findAllPageable) {
-        // TODO Auto-generated method stub
-
-    }
-
-    public boolean isNumberFormatException() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+//    public UserSearchResult(Page<UserDto> findAllPageable, boolean b) {
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//    public UserSearchResult() {
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//    public Page<UserDto> getUserPage() {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+//
+//    public void setUserPage(Page<UserDto> findAllPageable) {
+//        // TODO Auto-generated method stub
+//
+//    }
+//
+//    public boolean isNumberFormatException() {
+//        // TODO Auto-generated method stub
+//        return false;
+//    }
 }
